0322a5bea923bf891cc44d751ce84e40693031ed
Temesgen Geta <nigusgetamamo@gmail.com>
2026-05-12 05:10:02 +0300

    feat(db): export final database schema and metadata

---
+++ migrations/default/1778550767961_init_schema/up.sql
@@ -0,0 +1,249 @@
+SET check_function_bodies = false;
+CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
+
+CREATE TABLE public.booking_seats (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    booking_id uuid NOT NULL,
+    seat_id uuid NOT NULL
+);
+
+CREATE TABLE public.bookings (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    user_id uuid NOT NULL,
+    schedule_id uuid NOT NULL,
+    total_price numeric NOT NULL,
+    payment_status text DEFAULT 'pending'::text NOT NULL,
+    payment_method text,
+    booking_reference text NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE public.bookmarks (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    user_id uuid NOT NULL,
+    movie_id uuid NOT NULL
+);
+
+CREATE TABLE public.directors (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    image_url text
+);
+
+CREATE TABLE public.genres (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL
+);
+
+CREATE TABLE public.halls (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL
+);
+
+CREATE TABLE public.movie_directors (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    movie_id uuid NOT NULL,
+    director_id uuid NOT NULL
+);
+
+CREATE TABLE public.movie_genres (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    movie_id uuid NOT NULL,
+    genre_id uuid NOT NULL
+);
+
+CREATE TABLE public.movie_images (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    movie_id uuid NOT NULL,
+    image_url text NOT NULL
+);
+
+CREATE TABLE public.movie_stars (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    movie_id uuid NOT NULL,
+    star_id uuid NOT NULL
+);
+
+CREATE TABLE public.movies (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    title text NOT NULL,
+    description text NOT NULL,
+    main_image text NOT NULL,
+    release_date date,
+    duration_minutes integer,
+    rating_avg numeric DEFAULT 0,
+    status text DEFAULT 'now_showing'::text NOT NULL,
+    trailer_url text,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE public.ratings (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    user_id uuid NOT NULL,
+    movie_id uuid NOT NULL,
+    rating_value integer NOT NULL,
+    comment text,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    CONSTRAINT ratings_rating_value_check CHECK (((rating_value >= 1) AND (rating_value <= 5)))
+);
+
+CREATE TABLE public.schedules (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    movie_id uuid NOT NULL,
+    hall_id uuid NOT NULL,
+    start_time timestamp with time zone NOT NULL,
+    end_time timestamp with time zone NOT NULL,
+    price numeric NOT NULL
+);
+
+CREATE TABLE public.seats (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    hall_id uuid NOT NULL,
+    row_label text NOT NULL,
+    column_number integer NOT NULL,
+    type text DEFAULT 'standard'::text NOT NULL
+);
+
+CREATE TABLE public.stars (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    image_url text
+);
+
+CREATE TABLE public.users (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    full_name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    role text DEFAULT 'user'::text NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE ONLY public.booking_seats
+    ADD CONSTRAINT booking_seats_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT bookings_booking_reference_key UNIQUE (booking_reference);
+    
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT bookings_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.bookmarks
+    ADD CONSTRAINT bookmarks_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.bookmarks
+    ADD CONSTRAINT bookmarks_user_id_movie_id_key UNIQUE (user_id, movie_id);
+    
+ALTER TABLE ONLY public.directors
+    ADD CONSTRAINT directors_pkey PRIMARY KEY (id);
+    
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_name_key UNIQUE (name);
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_pkey PRIMARY KEY (id);
+    
+ALTER TABLE ONLY public.halls
+    ADD CONSTRAINT halls_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.movie_directors
+    ADD CONSTRAINT movie_directors_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.movie_genres
+    ADD CONSTRAINT movie_genres_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.movie_images
+    ADD CONSTRAINT movie_images_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.movie_stars
+    ADD CONSTRAINT movie_stars_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.seats
+    ADD CONSTRAINT seats_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.stars
+    ADD CONSTRAINT stars_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+CREATE TRIGGER set_movies_updated_at BEFORE UPDATE ON public.movies FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+CREATE TRIGGER set_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+
+ALTER TABLE ONLY public.booking_seats
+    ADD CONSTRAINT booking_seats_booking_id_fkey FOREIGN KEY (booking_id) REFERENCES public.bookings(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.booking_seats
+    ADD CONSTRAINT booking_seats_seat_id_fkey FOREIGN KEY (seat_id) REFERENCES public.seats(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT bookings_schedule_id_fkey FOREIGN KEY (schedule_id) REFERENCES public.schedules(id) ON DELETE CASCADE;
+    
+ALTER TABLE ONLY public.bookings
+    ADD CONSTRAINT bookings_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+    
+ALTER TABLE ONLY public.bookmarks
+    ADD CONSTRAINT bookmarks_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.bookmarks
+    ADD CONSTRAINT bookmarks_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.movie_directors
+    ADD CONSTRAINT movie_directors_director_id_fkey FOREIGN KEY (director_id) REFERENCES public.directors(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.movie_directors
+    ADD CONSTRAINT movie_directors_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.movie_genres
+    ADD CONSTRAINT movie_genres_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES public.genres(id) ON DELETE CASCADE;
+    
+ALTER TABLE ONLY public.movie_genres
+    ADD CONSTRAINT movie_genres_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.movie_images
+    ADD CONSTRAINT movie_images_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.movie_stars
+    ADD CONSTRAINT movie_stars_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+    
+ALTER TABLE ONLY public.movie_stars
+    ADD CONSTRAINT movie_stars_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.stars(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_hall_id_fkey FOREIGN KEY (hall_id) REFERENCES public.halls(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.seats
+    ADD CONSTRAINT seats_hall_id_fkey FOREIGN KEY (hall_id) REFERENCES public.halls(id) ON DELETE CASCADE;
